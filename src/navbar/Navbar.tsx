84b03757d59faa866add38cef79d5bb5d2a14cde
Nader Nasr <nader.nasr@memoryjar.com>
2023-04-25 10:27:36 -0400

    author tag

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
-function Navbar(): JSX.Element {
+function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.links}>
         <Link to="/" className={styles.link}>Ethereum</Link>
         <Link to="/polygon" className={styles.link}>Polygon</Link>
       </div>
+      <a href='https://www.nadernasr.ca' className={styles.button}>Made with 🫶 by Nader Nasr</a>
     </div>
   );
 }
