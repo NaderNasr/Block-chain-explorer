12c065a9ab185b465968193af68a48a0f97793c1
Nader Nasr <nader.nasr@memoryjar.com>
2023-05-02 01:11:47 -0400

    styling

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
-import logo from '../assets/Frame 1.png';
+import logo from '../assets/EtherAction.png';
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.links}>
-        <Link to='/' className={styles.link}>EtherAction</Link>
+        <Link to='/' className={styles.link}>
+          <img src={logo} alt='logo' className={styles.logo} />
+        </Link>
       </div>
       <a href='https://www.nadernasr.ca' className={styles.button}>Made with 🫶 Nader Nasr</a>
     </div>
